e59e900b54596c51d82d76dd3eb773391c9d9a78
rratneshSingh <work.ratnesh@gmail.com>
2025-05-29 22:58:24 +0530

    Initial commit

---
+++ src/main/java/in/naukriboost/naukriboost/NaukriboostApplication.java
@@ -0,0 +1,13 @@
+package in.naukriboost.naukriboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukriboostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaukriboostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/naukriboost/naukriboost/NaukriboostApplicationTests.java
@@ -0,0 +1,13 @@
+package in.naukriboost.naukriboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukriboostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
